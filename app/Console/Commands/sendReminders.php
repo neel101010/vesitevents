99721f7be50b668d59b98bfd1945373f58f3a155
Neel Bhagat <neelnb11@gmail.com>
2021-01-26 17:04:02 +0530

    update

--- app/Console/Commands/sendReminders.php
+++ app/Console/Commands/sendReminders.php
@@ -6,6 +6,7 @@ use App\Models\Event;
 use App\Models\Register;
 use App\Models\User;
 use Illuminate\Console\Command;
+use Twilio\Rest\Client;
 
 class sendReminders extends Command
 {
@@ -40,36 +41,34 @@ class sendReminders extends Command
      */
     public function handle()
     {
-//        $events = Event::where('date',date('Y-m-d'))->get();
-//        foreach ($events as $event){
-//            $users_registers = Register::where('event_id',$event->id)
-//                ->where('send_remainder',0)
-//                ->get();
-//            foreach ($users_registers as $users_register){
-//                $user = User::find($users_register->user_id);
-//                $users_register->send_remainder = 1;
-//                $basic  = new \Nexmo\Client\Credentials\Basic('208c20c2', 'dOEtRew2SEbYZwzS');
-//                $client = new \Nexmo\Client($basic);
-//                try{
-//                    $message = $client->message()->send([
-//                        'to' => $user->phone_number,
-//                        'from' => 'Vesit Events',
-//                        'text' => $event->name.' is scheduled on '. $event->date. ' at '. $event->time
-//                    ]);
-//                    $users_register->save();
-//                }
-//                catch (\Exception $e){
-//                    continue;
-//                }
-//            }
-//        }
-        $basic  = new \Nexmo\Client\Credentials\Basic('208c20c2', 'dOEtRew2SEbYZwzS');
-        $client = new \Nexmo\Client($basic);
-        $message = $client->message()->send([
-            'to' => '918291597204',
-            'from' => 'Vesit Events',
-            'text' =>'hi'
-        ]);
+        $events = Event::where('date',date('Y-m-d'))->get();
+        foreach ($events as $event){
+            $users_registers = Register::where('event_id',$event->id)
+                ->where('send_remainder',0)
+                ->get();
+            foreach($users_registers as $users_register){
+                $user = User::find($users_register->user_id);
+                $users_register->send_remainder = 1;
+
+                $account_sid = getenv("TWILIO_SID");
+                $auth_token = getenv("TWILIO_AUTH_TOKEN");
+                $twilio_number = getenv("TWILIO_NUMBER");
+                $client = new Client($account_sid, $auth_token);
+                $user_phn = "+91".$user->phone_number;
+                $bodytext = $event->name.' is scheduled on '. $event->date. ' at '. $event->time;
+                try{
+                    $client->messages->create(
+                        $user_phn,
+                        ['from' => $twilio_number, 'body' => $bodytext]);
+                    $users_register->save();
+
+                }
+
+                catch (\Exception $e){
+                    echo $e;
+                }
+            }
+        }
         $this->info('Successfully sent remainder');
     }
 }
