054688eb3a1ed8856bbd13136ebaf4808a89ddec
Neel Bhagat <neelnb11@gmail.com>
2021-01-31 22:29:50 +0530

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,24 +12,24 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-//    public function boot(UrlGenerator $url)
-//    {
-//        if (env('APP_ENV') !== 'local') {
-//            $url->forceScheme('https');
-//        }
-//    }
+    public function boot(UrlGenerator $url)
+    {
+        if (env('APP_ENV') !== 'local') {
+            $url->forceScheme('https');
+        }
+    }
     public function register()
     {
         //
     }
-
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
+//
+//    /**
+//     * Bootstrap any application services.
+//     *
+//     * @return void
+//     */
+//    public function boot()
+//    {
+//        //
+//    }
 }
